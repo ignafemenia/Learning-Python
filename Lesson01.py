eb03e73a0057868ece075cfdb2cce9ad536ddb0d
Ignacio Femenia <femeniax@gmail.com>
2022-06-27 11:19:41 -0300

    First Commit

---
+++ Lesson01.py
@@ -0,0 +1,34 @@
+print("Hola Mundo")
+
+miVariable = "Hola desde Python"
+print(miVariable)
+
+miVariable = 2
+print(miVariable)
+
+x = 10
+y = 2
+z = x + y
+print(z)
+
+# Como poner el nombre de las variables
+nombre = 'Juan Perez'
+telefono = 6432874
+email = 'jperez@gmail.com'
+print(nombre, telefono, email)
+
+# Tipos de datos
+x = 10
+print(x)
+print(type(x))
+
+# Manejo de cadenas
+miGrupoFavorito = 'Aerosmith'
+comentario = 'The best rock band'
+print('Mi grupo favorito es : ' + miGrupoFavorito + " " + comentario)
+print('Mi grupo favorito es :', miGrupoFavorito, comentario)
+
+numero1 = '1'
+numero2 = '2'
+print('concatenacion:', numero1 + numero2)
+print('Suma:', int(numero1) + int(numero2))
